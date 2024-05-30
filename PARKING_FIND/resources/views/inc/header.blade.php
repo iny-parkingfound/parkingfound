cf152900dd0f3ea70786186f0a237c5d741b0312
yoozeze <fbghrud3@naver.com>
2024-05-30 17:15:37 +0900

    20240530 footer2

--- PARKING_FIND/resources/views/inc/header.blade.php
+++ PARKING_FIND/resources/views/inc/header.blade.php
@@ -1,13 +1,13 @@
 <header>
     <div class="nav-container">
-        <a href="#">
+        <a href="{{ route('index') }}">
             <img src="/img/image.png" class="header-title"></img>
         </a>
         <ul class="nav" id="nav">
-            <li class="menu menu1"><a href="">HOME</a></li>
+            <li class="menu menu1"><a href="{{ route('index') }}">HOME</a></li>
             <li class="menu menu2"><a href="">주차장 검색</a></li>
-            <li class="menu menu3"><a href="">주차 예약</a></li>
-            <li class="menu menu4"><a href="">Q & A</a></li>
+            <li class="menu menu3"><a href="{{ route('reservation') }}">주차 예약</a></li>
+            <li class="menu menu4"><a href="{{ route('question') }}">Q & A</a></li>
             <li class="profile"><a href="">이현수님</a></li>
             <li class="profile-img"><a href=""><img src="/img/0.png" class="profile-img2"></a></li>
         </ul>
