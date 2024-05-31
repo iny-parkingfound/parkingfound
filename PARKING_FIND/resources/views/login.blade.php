ba8240258027b0c31c17efe1b6ea4c169c0337a9
haem1n2 <uehek1234@gmail.com>
2024-05-31 10:51:41 +0900

    240531 블레이드 완료 이현수 레츠고

---
+++ PARKING_FIND/resources/views/login.blade.php
@@ -0,0 +1,27 @@
+@extends('inc.layout')
+
+@section('privateCSS')
+<link rel="stylesheet" href="/css/login.css">
+@endsection
+
+@section('main')
+<form action="login" method="post">
+<div class="container">
+    <h2>로그인</h2>
+    <div class="main-login">
+    <div class="id">
+        <label class="username" for="username">아이디</label>
+        <input class="user-name" type="text" id="username" name="username">
+    </div>
+    <div class="pass">
+        <label class="userpass" for="password">비밀번호</label>
+        <input class="user-pass" type="password" id="password" name="password">
+    </div>
+    <div class="button">
+    <button type="submit" class="login">로그인</button>
+    <a href="{{ route('register') }}">회원가입</a>
+</div>
+</div>
+</div>
+</form>
+@endsection
