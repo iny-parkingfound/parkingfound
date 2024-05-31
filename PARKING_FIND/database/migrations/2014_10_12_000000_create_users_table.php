40eca0bf18470379d690c911767ed38ea44cb1c7
yoozeze <fbghrud3@naver.com>
2024-05-31 11:37:20 +0900

    20240531 register

--- PARKING_FIND/database/migrations/2014_10_12_000000_create_users_table.php
+++ PARKING_FIND/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('email', 50)->unique();
+            $table->string('name', 50);
+            $table->string('password', 256);
+            $table->bigInteger('phonenum')->unsigned();
+            $table->string('car', 50);
+            $table->string('carnum', 50);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
--- PARKING_FIND/resources/views/register.blade.php
+++ PARKING_FIND/resources/views/register.blade.php
@@ -5,58 +5,58 @@
 @endsection
 
 @section('main')
-<form action="register" method="post">
-    <div class="container">
-        <h2>회원가입</h2>
-        <div class="main-box">
+    <form action="register" method="post">
+        <div class="container">
+            <h2>회원가입</h2>
+            <div class="main-box">
+                <hr>
+                <div class="main-box-solo">
+                <div class="main-title">아이디</div>
+                <div class="main-content">이현수</div>
+                <button type="submit" class="main-button">중복체크</button>
+            </div>
             <hr>
             <div class="main-box-solo">
-            <div class="main-title">아이디</div>
-            <div class="main-content">이현수</div>
-            <button type="submit" class="main-button">중복체크</button>
+                <div class="main-title">비밀번호</div>
+                <div class="main-content">********</div>
+                <div></div>
+            </div>
+            <hr>
+            <div class="main-box-solo">
+                <div class="main-title">비밀번호 확인</div>
+                <div class="main-content">********</div>
+                <div></div>
+            </div>
+            <hr>
+            <div class="main-box-solo">
+                <div class="main-title">이름</div>
+                <div class="main-content">이현수수깡</div>
+                <div></div>
+            </div>
+            <hr>
+            <div class="main-box-solo">
+                <div class="main-title">휴대전화</div>
+                <div class="main-content">010-6275-4954</div>
+                <div></div>
+            </div>
+            <hr>
+            <div class="main-box-solo">
+                <div class="main-title">차종</div>
+                <div class="main-content">녹차</div>
+                <div></div>
+            </div>
+            <hr>
+            <div class="main-box-solo">
+                <div class="main-title">차번호</div>
+                <div class="main-content">녹차 2313</div>
+                <div></div>
+            </div>
+            <hr>
+            </div>
+            <div class="end">
+            <button type="submit" class="yes">확인</button>
+            <a href="{{ route('reserve') }}">취소</a>
         </div>
-        <hr>
-        <div class="main-box-solo">
-            <div class="main-title">비밀번호</div>
-            <div class="main-content">********</div>
-            <div></div>
         </div>
-        <hr>
-        <div class="main-box-solo">
-            <div class="main-title">비밀번호 확인</div>
-            <div class="main-content">********</div>
-            <div></div>
-        </div>
-        <hr>
-        <div class="main-box-solo">
-            <div class="main-title">이름</div>
-            <div class="main-content">이현수수깡</div>
-            <div></div>
-        </div>
-        <hr>
-        <div class="main-box-solo">
-            <div class="main-title">휴대전화</div>
-            <div class="main-content">010-6275-4954</div>
-            <div></div>
-        </div>
-        <hr>
-        <div class="main-box-solo">
-            <div class="main-title">차종</div>
-            <div class="main-content">녹차</div>
-            <div></div>
-        </div>
-        <hr>
-        <div class="main-box-solo">
-            <div class="main-title">차번호</div>
-            <div class="main-content">녹차 2313</div>
-            <div></div>
-        </div>
-        <hr>
-        </div>
-        <div class="end">
-        <button type="submit" class="yes">확인</button>
-        <a href="{{ route('reserve') }}">취소</a>
-    </div>
-    </div>
-</form>
+    </form>
 @endsection
