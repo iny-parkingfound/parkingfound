5951f00e71ecdc79771a811cdca1b108dfeddaf5
yoozeze <fbghrud3@naver.com>
2024-05-31 10:51:52 +0900

    20240531 header

--- PARKING_FIND/resources/views/inc/header.blade.php
+++ PARKING_FIND/resources/views/inc/header.blade.php
@@ -8,8 +8,8 @@
             <li class="menu menu2"><a href="">주차장 검색</a></li>
             <li class="menu menu3"><a href="{{ route('reservation') }}">주차 예약</a></li>
             <li class="menu menu4"><a href="{{ route('question') }}">Q & A</a></li>
-            <li class="profile"><a href="">이현수님</a></li>
-            <li class="profile-img"><a href=""><img src="/img/0.png" class="profile-img2"></a></li>
+            <li class="profile"><a href="{{ route('reserve') }}">이현수님</a></li>
+            <li class="profile-img"><a href="{{ route('reserve') }}"><img src="/img/0.png" class="profile-img2"></a></li>
         </ul>
     <img src="/img/ham2.png" class="iconham hamburger" id="hamburger">
     <img src="/img/x2.png" class="iconx" id="cancel">
